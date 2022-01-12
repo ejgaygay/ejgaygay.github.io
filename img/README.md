5daff0a35c7102043ec8e436ff9485e927107103
EJ Gaygay <eldyjohngaygay@yahoo.com>
2022-01-12 18:31:45 -0500

    fixing up background stuff

--- img/README.md
+++ img/README.md
@@ -1,5 +1,2 @@
 <h1>Royalty Free Images Used</h1>
-<img src = "bg2.jpeg"/>
-<a href="https://wallpapercave.com/wp/wp6269197.jpg">https://wallpapercave.com/wp/wp6269197.jpg</a>
-<img src = "bg3.jpg"/>
-<a href="https://images.squarespace-cdn.com/content/v1/60a77c9619f082755c08c47a/1638887566262-ZOEYLHICFN6AKJZEBU61/jordan-duca-aOqEXM_zI_4-unsplash.jpg">https://images.squarespace-cdn.com/content/v1/60a77c9619f082755c08c47a/1638887566262-ZOEYLHICFN6AKJZEBU61/jordan-duca-aOqEXM_zI_4-unsplash.jpg</a>
+<p>Out of fear for copyright and other legal issues, for now I don't have any images on the site, however I will add some soon.</p>
--- src/dynamicbg.js
+++ src/dynamicbg.js
@@ -1,3 +1,5 @@
+var background = "green";
+
 function changeBackground() {
 	//
 	var backgroundNumber = Math.floor(Math.random()*3) + 1;
@@ -5,6 +7,14 @@ function changeBackground() {
 	if (backgroundNumber == 0) {
 		console.log("got a zero");
 	}
+	document.body.style.backgroundColor = "black";
+	window.background = "black";
+	if (backgroundNumber == 1) {
+		document.body.style.backgroundColor = "red";
+		window.background = "red";
+	}
+
+	console.log(window.background);
 	//changes the background!
 	//directory starts at index.html location
 	//change up first background image
@@ -12,11 +22,20 @@ function changeBackground() {
 		console.log("here");
 		document.body.style.background = "url(img/bg2.jpeg)";
 	}*/
+	/*
 	if (backgroundNumber == 2){
 		document.body.style.background = "url(img/bg.jpg)";
 	}
 	else {
 		document.body.style.background = "url(img/bg3.jpg)";
 	}
+	*/
 
+}
+
+console.log(window.background);
+
+function newPage(){
+	//console.log(window.background);
+	document.body.style.backgroundColor = window.background;
 }
