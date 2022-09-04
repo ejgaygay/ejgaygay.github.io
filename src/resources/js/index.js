64d735ed8933459464f03110d847b7c98b653798
EJ Gaygay <ejgaygay@EJs-MacBook-Pro.fios-router.home>
2022-09-04 04:11:01 -0400

    docs

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -1,5 +1,6 @@
-console.log("sup");
 document.getElementById("tabs-top").scrollIntoView();
+
+//called by tabs being clicked.
 function gotoId(id) {
     console.log("hello");
     document.getElementById(id).scrollIntoView();
