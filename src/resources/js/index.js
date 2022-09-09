ae61a2f6bcd730c7548494d5edbcf4099a7cabbb
EJ Gaygay <ejgaygay@EJs-MacBook-Pro.fios-router.home>
2022-09-09 03:30:18 -0400

    video fix and responsive design

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -5,20 +5,3 @@ function gotoId(id) {
     console.log("hello");
     document.getElementById(id).scrollIntoView();
 }
-
-//get height of element
-console.log(document.getElementById("project-box-1").offsetHeight);
-console.log(document.getElementById("project-box-1").offsetWidth);
-
-function setVideoDimensions() {
-    console.log("resize");
-    var x = document.getElementById("project-box-1").offsetWidth;
-    var y = document.getElementById("project-box-1").offsetHeight;
-
-    //videos for projects 25px less each
-    x-=25;
-    y-=25;
-
-    document.getElementById("project-video1").setAttribute("height", y.toString());
-    document.getElementById("project-video1").setAttribute("width", x.toString());
-}
